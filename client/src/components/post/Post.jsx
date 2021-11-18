72f00041d28b02f556efd6a54207316e41c777d7
milleseseau <leojimlavi@gmail.com>
2021-11-18 00:17:28 -0500

    Fetching user inside userEffect with Axios

--- client/src/components/post/Post.jsx
+++ client/src/components/post/Post.jsx
@@ -1,12 +1,22 @@
 import "./post.css";
 import { MoreVert } from "@material-ui/icons";
-import { Users } from "../../dummyData";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { format } from "timeago.js";
 
 const Post = ({ post }) => {
-	const [like, setLike] = useState(post.like);
+	const [like, setLike] = useState(post.likes.length);
 	const [isLiked, setIsLiked] = useState(false);
+	const [user, setUser] = useState({});
 	const PF = process.env.REACT_APP_PUBLIC_FOLDER;
+
+	useEffect(() => {
+		const fetchUser = async () => {
+			const res = await axios.get(`users/${post.userId}`);
+			setUser(res.data);
+		};
+		fetchUser();
+	}, [post.userId]);
 
 	const likeHandler = () => {
 		setLike(isLiked ? like - 1 : like + 1);
@@ -20,13 +30,11 @@ const Post = ({ post }) => {
 					<div className="postTopLeft">
 						<img
 							className="postProfileImg"
-							src={Users.filter((u) => u.id === post?.userId)[0].profilePicture}
+							src={user.profilePicture || PF + "person/noAvatar.png"}
 							alt=""
 						/>
-						<span className="postUsername">
-							{Users.filter((u) => u.id === post.userId)[0].username}
-						</span>
-						<span className="postDate">{post.date}</span>
+						<span className="postUsername">{user.username}</span>
+						<span className="postDate">{format(post.createAt)}</span>
 					</div>
 					<div className="postTopRight">
 						<MoreVert />
@@ -34,7 +42,7 @@ const Post = ({ post }) => {
 				</div>
 				<div className="postCenter">
 					<span className="postText">{post?.desc}</span>
-					<img src={PF + post.photo} alt="" className="postImg" />
+					<img src={PF + post.img} alt="" className="postImg" />
 				</div>
 				<div className="postBottom">
 					<div className="postBottomLeft">
