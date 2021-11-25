f1fc5203ce633644037a4e37fb883c172ae1bae3
milleseseau <leojimlavi@gmail.com>
2021-11-24 21:45:33 -0500

    like and disliked

--- client/src/components/post/Post.jsx
+++ client/src/components/post/Post.jsx
@@ -1,15 +1,21 @@
 import "./post.css";
 import { MoreVert } from "@material-ui/icons";
-import { useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import axios from "axios";
 import { format } from "timeago.js";
 import { Link } from "react-router-dom";
+import { AuthContext } from "../../context/AuthContext";
 
 const Post = ({ post }) => {
 	const [like, setLike] = useState(post.likes.length);
 	const [isLiked, setIsLiked] = useState(false);
 	const [user, setUser] = useState({});
 	const PF = process.env.REACT_APP_PUBLIC_FOLDER;
+	const { user: currentUser } = useContext(AuthContext);
+
+	useEffect(() => {
+		setIsLiked(post.likes.includes(currentUser._id));
+	}, [currentUser, post.likes]);
 
 	useEffect(() => {
 		const fetchUser = async () => {
@@ -20,6 +26,9 @@ const Post = ({ post }) => {
 	}, [post.userId]);
 
 	const likeHandler = () => {
+		try {
+			axios.put("/posts/" + post._id + "/like", { userId: currentUser._id });
+		} catch (err) {}
 		setLike(isLiked ? like - 1 : like + 1);
 		setIsLiked(!isLiked);
 	};
