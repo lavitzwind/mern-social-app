c0de00f4f10ad08e5f7be61a82b88b4123f5de82
milleseseau <leojimlavi@gmail.com>
2021-11-29 00:02:22 -0500

    React image upload showing the image

--- client/src/components/share/Share.jsx
+++ client/src/components/share/Share.jsx
@@ -1,5 +1,11 @@
 import "./share.css";
-import { PermMedia, Label, Room, EmojiEmotions } from "@material-ui/icons";
+import {
+	PermMedia,
+	Label,
+	Room,
+	EmojiEmotions,
+	Cancel,
+} from "@material-ui/icons";
 import { useContext, useRef, useState } from "react";
 import { AuthContext } from "../../context/AuthContext";
 import axios from "axios";
@@ -56,6 +62,12 @@ const Share = () => {
 					/>
 				</div>
 				<hr className="shareHr" />
+				{file && (
+					<div className="shareImgContainer">
+						<img src={URL.createObjectURL(file)} alt="" className="shareImg" />
+						<Cancel className="shareCancelImg" onClick={() => setFile(null)} />
+					</div>
+				)}
 				<form className="shareBottom" onSubmit={submitHandler}>
 					<div className="shareOptions">
 						<label htmlFor="file" className="shareOption">
